6c787d63bd5f99705b4436194d196730c0717fb1
Jordan Brown <jmbrown@andrew.cmu.edu>
2015-09-05 22:12:46 -0400

    fuck this already

--- FeedMe/FeedMe/MyRecipesViewController.swift
+++ FeedMe/FeedMe/MyRecipesViewController.swift
@@ -7,17 +7,15 @@
 //
 
 import UIKit
-import Koloda
-import pop
+
 private var numberOfCards: UInt = 5
-class MyRecipesViewController: UIViewController, UITextFieldDelegate, KolodaViewDataSource, KolodaViewDelegate {
+class MyRecipesViewController: UIViewController, UITextFieldDelegate {
 
     var foodImage : UIImage? = nil;
     var localUser : User = User();
     
     @IBOutlet var textField : UITextField!
-    @IBOutlet weak var kolodaView: KolodaView!
-    
+    @IBOutlet var recipeView : UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,11 +23,7 @@ class MyRecipesViewController: UIViewController, UITextFieldDelegate, KolodaView
         // Handle the text field’s user input through delegate callbacks.
         textField.delegate = self
         var tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "DismissKeyboard")
-        view.addGestureRecognizer(tap)
-        kolodaView.dataSource = self
-        kolodaView.delegate = self
-        
-        self.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+        self.view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
@@ -57,67 +51,4 @@ class MyRecipesViewController: UIViewController, UITextFieldDelegate, KolodaView
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }
-
-    //MARK: IBActions
-    @IBAction func leftButtonTapped() {
-        kolodaView?.swipe(SwipeResultDirection.Left)
-    }
-    
-    @IBAction func rightButtonTapped() {
-        kolodaView?.swipe(SwipeResultDirection.Right)
-    }
-    
-    @IBAction func undoButtonTapped() {
-        kolodaView?.revertAction()
-    }
-    
-    //MARK: KolodaViewDataSource
-    func kolodaNumberOfCards(koloda: KolodaView) -> UInt {
-        return numberOfCards
-    }
-    
-    func kolodaViewForCardAtIndex(koloda: KolodaView, index: UInt) -> UIView {
-        return UIImageView(image: UIImage(named: "Card_like_\(index + 1)"))
-    }
-    func kolodaViewForCardOverlayAtIndex(koloda: KolodaView, index: UInt) -> OverlayView? {
-        return NSBundle.mainBundle().loadNibNamed("OverlayView",
-            owner: self, options: nil)[0] as? OverlayView
-    }
-    
-    //MARK: KolodaViewDelegate
-    
-    func kolodaDidSwipedCardAtIndex(koloda: KolodaView, index: UInt, direction: SwipeResultDirection) {
-        //Example: loading more cards
-        if index >= 3 {
-            numberOfCards = 6
-            kolodaView.reloadData()
-        }
-    }
-    
-    func kolodaDidRunOutOfCards(koloda: KolodaView) {
-        //Example: reloading
-        kolodaView.resetCurrentCardNumber()
-    }
-    
-    func kolodaDidSelectCardAtIndex(koloda: KolodaView, index: UInt) {
-        UIApplication.sharedApplication().openURL(NSURL(string: "http://yalantis.com/")!)
-    }
-    
-    func kolodaShouldApplyAppearAnimation(koloda: KolodaView) -> Bool {
-        return true
-    }
-    
-    func kolodaShouldMoveBackgroundCard(koloda: KolodaView) -> Bool {
-        return true
-    }
-    
-    func kolodaShouldTransparentizeNextCard(koloda: KolodaView) -> Bool {
-        return true
-    }
-    
-    func kolodaBackgroundCardAnimation(koloda: KolodaView) -> POPPropertyAnimation? {
-        return nil
-    }
-
-
 }
